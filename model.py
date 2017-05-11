eae7199d0992bdaf14a8e06c33809a8c3b39a323
emiranda927 <eliseo.l.miranda@gmail.com>
2017-05-11 19:20:53 -0400

    Add files via upload

--- model.py
+++ model.py
@@ -82,7 +82,7 @@ from keras.layers import Conv2D, MaxPooling2D, Cropping2D, GaussianDropout
 from keras.layers.advanced_activations import LeakyReLU
 model = Sequential()
 model.add(Cropping2D(cropping=((65,20), (0,0)), input_shape=(160,320,3)))
-model.add(Lambda(lambda x: (x/255.0) - 0.5))
+model.add(Lambda(lambda x: (x/255.0) - 0.5)) #Normalize images
 model.add(Conv2D(filters=24, kernel_size=(5,5), strides=(2,2)))
 model.add(LeakyReLU(alpha=0.3)) #LeakyReLU appeared to make the vehicle drive more smoothly
 model.add(Conv2D(filters=36, kernel_size=(5,5), strides=(2,2)))
@@ -102,7 +102,7 @@ model.add(Dropout(rate=0.5)) #Dropout
 model.add(Dense(10))
 model.add(Dense(1))
 
-model.compile(optimizer='adam', loss='mse')
+model.compile(optimizer='adam', loss='mse') #used adam optimizer for parameter tuning
 
 #fit model
 history = model.fit_generator(train_generator, steps_per_epoch= \
